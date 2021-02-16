97c183886cb497c86ccffbad2dfdd2b2f6d87ffb
Daniel Persson <daniel.persson18@learnet.se>
2021-02-16 08:01:06 +0100

    Fixat så bild fungerar och lagt till kommentarer

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
 
 /* 1:
 Hade velat skriva något vettigt, men jag har inte fått rätt på det.
-
+Använder debuggern under fler verktyg, för programmerare i Chrome.
 
 */
 document.addEventListener("DOMContentLoaded", onPageload);
 
 function onPageload(){
-    let input_number = document.querySelector("#input-number");
+    let input_number = document.querySelector("#input-number"); // Har lagt till # så att den kan använda Id och ändrat Input-number till input-number...
 
     input_number.oninput = function(){
         resetSections();
@@ -29,24 +29,23 @@ function createSections(count){ // Parent
    // let count = parseInt(this.value);
     for(let i = 0; i < count; i++){
 
-       // let section = document.createElement("div");
-       // section.setAttribute("id", "section")
-        //let sectionParent = document.getElementById("sections");
+        // let section = document.createElement("div");
+        // section.setAttribute("id", "section")
+        // let sectionParent = document.getElementById("sections");
 
-            let sectionParent = document.querySelector("main");
-            let child = document.createElement("section");             // Använder DOM för att skapa en section
-            let title = document.createElement("h2");                  // Använder DOM för att skapa ett h-element
-            let picture = document.createElement('img');               // Använder DOM för att skapa en plats för en bild
-           // img.src =  'img/Blog_Bild.jpg';
-            let blogtext = document.createElement("p2");               // Använder DOM för att skapa ett p paragraph text, som kan fungera som blogtext
+            let sectionParent = document.querySelector("main")
+            let child = document.createElement("section")            // Använder DOM för att skapa ett section Element
+            let title = document.createElement("h2")                 // Använder DOM för att skapa ett h element
+            let picture = document.createElement('img')               // Använder DOM för att skapa en plats för en bild element
+            let blogtext = document.createElement("p2")               // Använder DOM för att skapa ett p paragraph element, som kan fungera som blogtext
 
-            title.innerHTML = "Title " + (i + 1);
-            //picture.innerHTML = "<img src=\"img/Blog_Bild.jpg\">";
+            title.innerHTML = "Title " + (i + 1)      // lägger in info i elementen
+            picture.setAttribute("src", "img/Blog_Bild.jpg")         // Lägger in en bild
             blogtext.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore harum, quasi dicta ex totam quisquam quo tempore maxime, commodi praesentium eius quod suscipit! Tenetur magnam eligendi amet fugiat adipisci impedit.";
             title.contentEditable = true; // Gör så att title och blogtext går att ändra...
             blogtext.contentEditable = true;
 
-
+            // Lägger till elementen som vi har skapat
             child.appendChild(title);
             child.appendChild(picture);
             child.appendChild(blogtext);
@@ -64,14 +63,16 @@ function createSections(count){ // Parent
 
 
 
-// HAr försökt få så att children och parents fungerar, men det har inte blivit bra...
+// Har försökt få så att children och parents fungerar, men det har inte blivit bra... DET ÄR BÄTTRE NU - men inte helt fixat
 function resetSections(){
-
-    let sectionParent = document.getElementById("sections")
+    
+    let sectionParent = document.getElementById("sections");
 
     let sectionChildren = sectionParent.childNodes;            // HAR ÄNDRAT sectionChild till sectionChildren här nere.
     for(let i = sectionChildren.length-1; i >= 0; i--){
-        let sectionChildren = sectionChildren[i];
-        sectionParent.removeChild(sectionChildren);            //bytade till sectionParent, för det gjorde samma sak...
+        let sectionChild = sectionChildren[i];
+        sectionChild.remove();            //bytade till sectionParent, för det gjorde samma sak... bytamde removeChild(sectionChildren) till removeChild(sectionChild)... nu kan jag skapa men inte radera inlägg...
     };
 };
+
+// sectionParent.removeChild(sectionChild);  
